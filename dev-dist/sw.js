b61860686d9898082df7d4e2940fd050be04ad95
IDGS-803-22001449 <81856@alumnos.utleon.edu.mx>
2025-12-02 12:02:53 -0600

    logoCorregido

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-de42eff6'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.1ki0isj983g"
+    "revision": "0.pj6rd2dgoeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
