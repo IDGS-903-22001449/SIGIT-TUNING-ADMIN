8d7941802b14390d28a572ed06b223c4a56b5a0c
IDGS-903-22001449 <81856@alumnos.utleon.edu.mx>
2025-12-02 20:03:29 -0600

    buscador2

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -7,11 +7,7 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="header-search">
-        <div className="search-icon">
-        </div>
-        
-      </div>
+      
 
       <div className="header-actions">
         
